342d339262c6899158f99c6dc44c0b5cc88eec2e
Anita Graham <anita@joli.com.au>
2019-04-27 12:22:35 +0800

    TimeSplitter gem needs a specific github version,  removed from  gemspec and put back into Gemfile

--- app/models/refinery/calendar/event.rb
+++ app/models/refinery/calendar/event.rb
@@ -1,58 +1,56 @@
 module Refinery
   module Calendar
     class Event < ActiveRecord::Base
-    include ActiveModel::AttributeMethods
+      include ActiveModel::AttributeMethods
 
-    extend TimeSplitter::Accessors
+      extend TimeSplitter::Accessors
+      extend FriendlyId
 
-    extend FriendlyId
+      split_accessor :from, :to,
+                     date_format: Refinery::Calendar.ruby_date_format,
+                     time_format: Refinery::Calendar.ruby_time_format
 
-    split_accessor :from, :to,
-                   date_format: Refinery::Calendar.ruby_date_format,
-                   time_format: Refinery::Calendar.ruby_time_format
+      friendly_id :title, :use => :slugged
 
-    friendly_id :title, :use => :slugged
+      belongs_to :venue
 
-    belongs_to :venue
-    dragonfly_accessor :main_image
+      dragonfly_accessor :main_image
+      has_many_page_images
 
-    has_many_page_images
+      validates :title, :presence => true, :uniqueness => true
 
-    validates :title, :presence => true, :uniqueness => true
+      alias_attribute :from, :starts_at
+      alias_attribute :to, :ends_at
 
-    alias_attribute :from, :starts_at
-    alias_attribute :to, :ends_at
+      delegate :name, :address,
+                to: :venue,
+                prefix: true,
+                allow_nil: true
 
-    delegate :name, :address,
-              to: :venue,
-              prefix: true,
-              allow_nil: true
+      scope :starting_on_day, lambda {|day| where(starts_at: day.beginning_of_day..day.tomorrow.beginning_of_day) }
+      scope :ending_on_day, lambda {|day| where(ends_at: day.beginning_of_day..day.tomorrow.beginning_of_day) }
 
-    scope :starting_on_day, lambda {|day| where(starts_at: day.beginning_of_day..day.tomorrow.beginning_of_day) }
-    scope :ending_on_day, lambda {|day| where(ends_at: day.beginning_of_day..day.tomorrow.beginning_of_day) }
+      scope :on_day, lambda {|day|
+        where(
+          arel_table[:starts_at].in(day.beginning_of_day..day.tomorrow.beginning_of_day).
+            or(arel_table[:ends_at].in(day.beginning_of_day..day.tomorrow.beginning_of_day)).
+            or( arel_table[:starts_at].lt(day.beginning_of_day).and(arel_table[:ends_at].gt(day.tomorrow.beginning_of_day)) )
+        )
+      }
+      scope :featured, -> { where featured: true }
+      scope :upcoming, -> { where arel_table[:starts_at].gteq Time.now }
+      scope :archive, ->  { where arel_table[:starts_at].lt Time.now }
 
-    scope :on_day, lambda {|day|
-      where(
-        arel_table[:starts_at].in(day.beginning_of_day..day.tomorrow.beginning_of_day).
-          or(arel_table[:ends_at].in(day.beginning_of_day..day.tomorrow.beginning_of_day)).
-          or( arel_table[:starts_at].lt(day.beginning_of_day).and(arel_table[:ends_at].gt(day.tomorrow.beginning_of_day)) )
-      )
-    }
-    scope :featured, -> { where featured: true }
-    scope :upcoming, -> { where arel_table[:starts_at].gteq Time.now }
-    scope :archive, ->  { where arel_table[:starts_at].lt Time.now }
-
-    def tense
-      case
-      when from.future?
-        'future'
-      when to.past?
-        'past'
-      when from.past? && to.future?
-        'present'
+      def tense
+        case
+        when from.future?
+          'future'
+        when to.past?
+          'past'
+        when from.past? && to.future?
+          'present'
+        end
       end
-    end
-
     end
   end
 end
